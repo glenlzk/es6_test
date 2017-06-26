b9c53ba31204c9bc51170ebb60b876b7e6d6f27c
glen <512043920@qq.com>
2017-06-26 11:30:25 +0800

    add demo

---
+++ example.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const REGEX = /a/y;
+
+// 指定从2号位置开始匹配
+REGEX.lastIndex = 2;
+
+// 不是粘连，匹配失败
+REGEX.exec('xaya') // null
+
+// 指定从3号位置开始匹配
+REGEX.lastIndex = 3;
+
+// 3号位置是粘连，匹配成功
+const match = REGEX.exec('xaxa');
+match.index // 3
+REGEX.lastIndex // 4
---
+++ src/example.js
@@ -0,0 +1,30 @@
+
+function compile(template){
+    var evalExpr = /<%=(.+?)%>/g;
+    var expr = /<%([\s\S]+?)%>/g;
+
+    template = template
+        .replace(evalExpr, '`); \n  echo( $1 ); \n  echo(`')
+        .replace(expr, '`); \n $1 \n  echo(`');
+
+    template = 'echo(`' + template + '`);';
+
+    var script =
+        `(function parse(data){
+    var output = "";
+
+    function echo(html){
+      output += html;
+    }
+
+    ${ template }
+
+    return output;
+  })`;
+
+    return script;
+}
+
+var parse = eval(compile(template));
+
+console.log( parse({ supplies: [ "broom", "mop", "cleaner" ] }));
